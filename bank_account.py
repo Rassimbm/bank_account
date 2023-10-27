f6cf9e5d406c968340c46131424521388f83373a
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-10-26 23:31:35 -0500

    created class Bank_account, the constructor method and four instance methods

--- bank_account.py
+++ bank_account.py
@@ -1,8 +1,34 @@
-#deposit(self, amount)
-#increases the account balance by the given amount
-#withdraw(self, amount)
-#decreases the account balance by the given amount if there are sufficient funds; if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
-#display_account_info(self)
-#print to the console: eg. "Balance: $100"
-#yield_interest(self)
-#increases the account balance by the current balance * the interest rate (as long as the balance is positive)
+class Bank_account:
+    interest_rate = 0.03
+    balance = 0
+
+    def __init__(self, interest_rate, balance):
+        self.interest_rate = interest_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        #increases the account balance by the given amount
+        self.balance += amount
+        print(f'Your new balance is: ${self.balance}.')
+        return self
+        
+    def withdraw(self, amount):
+        # decreases the account balance by the given amount if there are sufficient funds; if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
+        if self.balance < amount:
+            print('Insufficient funds: Charging a $5 fee')
+            self.balance -= 5
+            return self
+        self.balance -= amount
+        return self
+    
+    def display_account_info(self):
+        #print to the console: eg. "Balance: $100"
+        print(f'Your available balance to use is: ${self.balance}')
+        return self
+
+    def yield_interest(self):
+        #increases the account balance by the current balance * the interest rate (as long as the balance is positive)
+        if self.balance > 0:
+            self.balance += (self.balance * Bank_account.interest_rate)
+        return self
+    
